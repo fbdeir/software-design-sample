ff153f3bcddac94d14e854d90017781cce918ade
fatima.bdeir <fatima.bdeir@montymobile.com>
2020-09-21 21:05:29 +0300

    initial commit

---
+++ src/main/java/Palindrome.java
@@ -0,0 +1,21 @@
+public class Palindrome {
+
+    /**
+     *Given a string, finds if the string is a palindrome or not.
+     * @param string
+     * @return boolean representing if the string is a palindrome or not
+     */
+    public static boolean isPalindrome(String string) {
+        if (string.length() == 0 || string.length() == 1) {
+            return true;
+        }
+        if (string.charAt(0) == string.charAt(string.length() - 1)) {
+            return isPalindrome(string.substring(1, string.length() - 1));
+        }
+        return false;
+    }
+
+    public static void main(String args[]){
+        System.out.println(isPalindrome("abab"));
+    }
+}
